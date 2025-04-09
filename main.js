4b067848ba24e40dd78ff911650e04eac30ab6b7
Mostafa <memo605.work@gmail.com>
2025-04-09 20:21:35 +0200

    Add human and computer score variables.

--- main.js
+++ main.js
@@ -22,9 +22,15 @@ function getHumanChoice() {
         alert ("Please enter a valid choice!")
     }
 }
-
 console.log(getHumanChoice());
 
+// the computer will return a human score and computer score after every round
+let humanScore = (0)
+let computerScore = (0)
+console.log("humanScore=",humanScore,"computerScore=",computerScore);
+
+
+// compare the human choice to the computer choice
 
 
 
@@ -32,10 +38,9 @@ console.log(getHumanChoice());
 
 
 // prompt pop up and player enters: rock or paper or scissors
-// compare the human choice to the computer choice
 // the computer will take the player choice and store it in a variable 
 // the computer will choose randomly between rock , paper or scissors
-// the computer will return a human score and computer score after every round
+
 // after each round the computer returns either: you win! or you lose! 
 // console returns the reason why you have won or lost like: Paper beats Rock
 // the game will consist of 5 rounds after it ends the console will declare the winner
